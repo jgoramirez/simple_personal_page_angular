85d2cf044f4ff8e2c081858df4a521daff2a77c1
Jose Guadalupe Omar Ramirez Mata <omarin92@gmail.com>
2022-03-23 01:45:49 -0600

    Corrección a icons
    Se incorpora el uso de icons bootstrap y se modifica el icono de la página

--- src/app/components/links/links-mock.ts
+++ src/app/components/links/links-mock.ts
@@ -1,6 +1,7 @@
 import { Links } from "./links.interface";
 
 export const LINKS: Links[] = [
-    { id: 1, url: "https://github.com/jgoramirez", icon: "faGithub" },
-    { id: 2, url: "https://www.linkedin.com/in/omarramirezmata/", icon: "faLinkedin" }
+    { id: 1, url: "https://www.linkedin.com/in/omarramirezmata/", icon: "linkedin" },
+    { id: 2, url: "https://github.com/jgoramirez", icon: "github" },
+    { id: 3, url: "https://www.facebook.com/jgoramirezmata", icon: "facebook"}
 ];
